961a030d807517e14a4e06f7b722b5dabc37e573
Kun Huang <kkun.huang@gmail.com>
2018-01-09 11:52:04 -0500

    Initial Commit

---
+++ RecognitionML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecognitionML
+//
+//  Created by Kun Huang on 1/9/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
